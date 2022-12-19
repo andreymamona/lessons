de13ea29e7cf3d6bbed95ecce34e1435c2354bf2
Andrey Mamona <andrey.jbjv@gmail.com>
2022-12-19 21:58:13 +0300

    exercises 4,5,6,7 add

---
+++ lesson03/classword5.py
@@ -0,0 +1,15 @@
+import math
+
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+
+my_set = set(my_list)
+
+print(len (my_list) - len (my_set))
+
+print(my_list[2:5][::-1])
+
+a = 4
+
+sqr_list = [a*4, a*a, (2*a*a)**0.5 ]
+
+print(sqr_list)
