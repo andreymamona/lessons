8ff21422a9ad51af41b05746ff4f85cc53294282
Andrey Mamona <andrey.jbjv@gmail.com>
2022-12-20 21:16:42 +0300

    Exercises 1 and 2

---
+++ lesson03/homework.py
@@ -0,0 +1,27 @@
+# N1. x = 2 + 2 * 2 - 2 / 2
+
+x = 2 + 2 * (2 - 2) / 2
+print(x)
+y = ((2 + 2 * 2) - 2) / 2
+print(y)
+
+# N2.
+
+my_list = ["P", "y", "t", "h", "o", "n"]
+
+my_str = str()
+
+for i in range(len(my_list)) :
+    my_str = my_str + my_list[i]
+
+inv_str = my_str [::-1]
+
+print("Оригинальный список:")
+print(my_list)
+print("Строка из списка")
+print(my_str)
+print("Инвертированная строка")
+print(inv_str)
+
+# N3.
+
