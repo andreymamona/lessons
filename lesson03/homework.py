a3c4ca6277160e53242e2efd3709a8036d2f8951
Andrey Mamona <andrey.jbjv@gmail.com>
2022-12-20 22:31:29 +0300

    All exercises done

--- lesson03/homework.py
+++ lesson03/homework.py
@@ -1,8 +1,10 @@
 # N1. x = 2 + 2 * 2 - 2 / 2
 
 x = 2 + 2 * (2 - 2) / 2
+print("x = 2 + 2 * (2 - 2) / 2")
 print(x)
 y = ((2 + 2 * 2) - 2) / 2
+print("y = ((2 + 2 * 2) - 2) / 2")
 print(y)
 
 # N2.
@@ -25,3 +27,16 @@ print(inv_str)
 
 # N3.
 
+deposit= 2130
+term = 5
+percent = 10
+bonus = 120
+
+result = deposit
+year = 1
+
+while year <= term:
+       result = result*1.1 + 120
+       year += 1
+
+print(result)
