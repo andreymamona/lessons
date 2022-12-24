56f4f8a10c34f7cc9a8733cf1cd1232ffd8256a4
Andrey Mamona <andrey.jbjv@gmail.com>
2022-12-24 16:24:26 +0300

    Classwork06 done!!!

--- lesson04/classwork06.py
+++ lesson04/classwork06.py
@@ -1,8 +1,26 @@
 # Вывести в порядке возрастания все простые числа, расположенные между n и m
 # (включая сами числа n и m), а также количество x этих чисел.
 
-n = int(input("Input n:"))
-m = int(input("Input m:"))
-l = m-n+1
-for i in range(l):
-    for j in range(n+1):
+n1 = int(input("Input n:"))
+m1 = int(input("Input m:"))
+x = int(0)  # quantity of simple numbers
+list_of_simple = list()
+
+for i in range(n1, m1+1):
+    if i == 1:if n1 == 1:
+    test = int(1)
+else:
+        test = int(1)
+    else:
+        test = int(0)
+    for j in range(1, i):
+        if i % j == 0:
+            test +=1
+    if test == 1:
+        x += 1
+        list_of_simple.append(i)
+
+print("First number: ", n1)
+print("Last number: ", m1)
+print("Quantity of simple numbers in range: ", x)
+print(list_of_simple)
