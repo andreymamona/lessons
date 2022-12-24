ac85a2ec95fe5dc80716ad5fcd15e3df5e92a9bd
Andrey Mamona <andrey.jbjv@gmail.com>
2022-12-24 12:03:53 +0300

    Edit formatting of classwork

--- lesson04/classwork05.py
+++ lesson04/classwork05.py
@@ -1,9 +1,11 @@
+# Получить сумму кубов натуральных чисел от n до m используя цикл for, числа n и m вводятся с клавиатуры.
+
 n = int(input("Input n"))
 m = int(input("Input m"))
 sum = 0
-if n > m :
+if n > m:
     print("!!! m > n")
 else:
-    for n in range(n,m+1):
+    for n in range(n, m+1):
         sum += n**3
 print(sum)
