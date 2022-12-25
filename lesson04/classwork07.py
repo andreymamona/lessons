97585e95481d919af1498d0c044106a58e337c80
Andrey Mamona <andrey.jbjv@gmail.com>
2022-12-25 18:56:59 +0300

    Classwork07 finished

--- lesson04/classwork07.py
+++ lesson04/classwork07.py
@@ -8,3 +8,40 @@ list_number = int(0)
 first_list = list()
 second_list = list()
 third_list = list()
+
+while True:
+    num = input("Input number: ")
+    if num.isdigit() == True:
+        if list_number == 0:
+            first_list.append(num)
+        elif list_number == 1:
+            second_list.append(num)
+        else:
+            third_list.append(num)
+    else:
+        list_number += 1
+        if list_number > 2:
+            break
+
+print(first_list)
+print(second_list)
+print(third_list)
+
+check = False
+result_list = list()
+
+for item1 in first_list:
+    for item2 in second_list:
+        if item1 == item2:
+            check = False
+            for item3 in third_list:
+                if item1 == item3:
+                    check = False
+                    break
+                else:
+                    check = True
+            if check == True:
+                result_list.append(item1)
+            break
+
+print(result_list)
