5da706194d65da8e6de12129bc4f00b8ab3468ca
Andrey Mamona <andrey.jbjv@gmail.com>
2023-01-03 23:02:56 +0300

    solution_03.py is finished (almost?)

--- lesson05/solution_03.py
+++ lesson05/solution_03.py
@@ -2,16 +2,23 @@
 # на файловой системе, функция имеет два параметра: ссылка на файл и имя на файловой системе.
 # В качестве примера ссылки на файл можно использовать лицензию из ГитХаба из вашего репозитория:
 # https://raw.githubusercontent.com/andreymamona/lessons/main/LICENSE
-
 import requests
 
+
+def download_file(my_url, file_name):
+    temp = requests.get(my_url)
+    my_file = open(file_name, 'w')
+    print(temp.text, '\n', 'Downloaded from:', temp.url, file=my_file)
+    my_file.close()
+
+
 default_address = 'https://raw.githubusercontent.com/andreymamona/lessons/main/LICENSE'
+default_file = 'print.txt'
+address = input('Input URL or d for default: ')
+if address == 'd':
+    address = default_address
+file = input('Input filename or d for default: ')
+if file == 'd':
+    file = default_file
 
-
-
-test = requests.get('https://raw.githubusercontent.com/andreymamona/lessons/main/LICENSE')
-print(test.text)
-
-my_file = open('print.txt', 'w')
-print(test.text, '\n', 'Downloaded from:', test.url, file=my_file )
-my_file.close()
+download_file(address, file)
