16b46dca3ff9df8d6e01647d7b44e6b4901f1896
Andrey Mamona <andrey.jbjv@gmail.com>
2023-01-11 20:08:03 +0300

    solution_02.py really done :)

--- lesson07/solution_02.py
+++ lesson07/solution_02.py
@@ -6,31 +6,37 @@ import csv
 
 
 def country_search(city_name):
-    city_info = {'name': city_name, 'region': None, 'country': None}
+    list_of_cities = []
     with open("cities/city.csv", "r") as file:
         reader = csv.reader(file)
         for row in reader:
+            city_info = {'name': city_name, 'region': None, 'country': None}
             if row[3] == city_info['name']:
                 city_info['region'] = row[2]
                 city_info['country'] = row[1]
-                # print(city_info)
+                list_of_cities.append(city_info)
+                # print(list_of_cities)
                 # break
-            if row[0] == "END":
+            if row[0] == "END" and list_of_cities is None:
                 print('Город не найден')
     with open("cities/region.csv", "r") as file:
         reader = csv.reader(file)
         for row in reader:
-            if row[0] == city_info['region']:
-                city_info['region'] = row[3]
-                # print(city_info)
-                break
+            for l in range(len(list_of_cities)):
+                if row[0] == list_of_cities[l]['region']:
+                    list_of_cities[l]['region'] = row[3]
+                    # print(city_info)
+                    # break
     with open("cities/country.csv", "r") as file:
         reader = csv.reader(file)
         for row in reader:
-            if row[0] == city_info['country']:
-                city_info['country'] = row[2]
-                print(city_info)
-                break
+            for l in range(len(list_of_cities)):
+                 if row[0] == list_of_cities[l]['country']:
+                    list_of_cities[l]['country'] = row[2]
+                    # print(city_info)
+                    # break
+    print(*(f'{value}' for value in list_of_cities if value), sep='\n ', end='.\n')
+    #print(list_of_cities)
 
 
 if __name__ == '__main__':
